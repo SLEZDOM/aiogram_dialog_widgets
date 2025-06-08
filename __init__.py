ae64d9572ef3872d42c54200e2226754f8263c23
slez dom <leeveepboy@gmail.com>
2025-06-09 01:46:33 +0300

    eighth commit

--- __init__.py
+++ __init__.py
@@ -1,30 +1,14 @@
-from .cancel import Cancel
-from .pagination_pager import PaginationPager, PaginationMode
-from .calendar import (
-    CustomCalendar,
-    MultiselectCalendar,
-    RadioCalendar,
-    MarkedCalendar
-)
+from .pagination import Pagination, PaginationMode
 from .tab import TabStart, TabSwitchTo, CheckStateMode
-from .switch_inline_query_current_chat import (
-    SwitchInlineQueryCurrentChat
-)
 from .rating import Rating, ManagedRating
 
 
 __all__ = [
-    "Cancel",
-    "PaginationPager",
+    "Pagination",
     "PaginationMode",
-    "CustomCalendar",
-    "MultiselectCalendar",
-    "RadioCalendar",
-    "MarkedCalendar",
     "TabStart",
     "TabSwitchTo",
     "CheckStateMode",
-    "SwitchInlineQueryCurrentChat",
     "Rating",
     "ManagedRating"
 ]
